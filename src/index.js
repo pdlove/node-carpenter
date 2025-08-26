b8b0eb0b82226d82e02d2ea6a9db51fdcf9f3da2
Paul Lovelace <pdlovelace@gmail.com>
2025-08-26 09:01:30 -0500

    Return Sequelize Op as well.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import CarpenterServer from "./lib/CarpenterServer.js";
 import CarpenterModel from "./lib/CarpenterModel.js";
-import { Sequelize, DataTypes } from "sequelize";
+import { Sequelize, DataTypes, Op } from "sequelize";
 import CarpenterModelRelationship from "./lib/CarpenterModelRelationship.js";
 import CarpenterRoute from "./lib/CarpenterRoute.js";
 const CarpenterJob = null; // Placeholder for CarpenterJob, as it is not defined yet
 
-export { CarpenterServer, CarpenterModel, Sequelize, DataTypes, CarpenterModelRelationship, CarpenterRoute, CarpenterJob };
+export { CarpenterServer, CarpenterModel, Sequelize, DataTypes, Op, CarpenterModelRelationship, CarpenterRoute, CarpenterJob };
